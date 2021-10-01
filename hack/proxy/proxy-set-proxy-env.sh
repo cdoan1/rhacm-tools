c2182867cd813180304dfec8680504feaf810a91
Chris Doan <cdoan@redhat.com>
2021-10-01 08:29:42 -0500

    update script

--- hack/proxy/proxy-set-proxy-env.sh
+++ hack/proxy/proxy-set-proxy-env.sh
@@ -1,53 +1,52 @@
 #!/bin/bash
 
-export xHTTP_PROXY=$(oc get proxy cluster -n openshift-config -ojsonpath='{.status.httpProxy}')
-export xHTTPS_PROXY=$(oc get proxy cluster -n openshift-config -ojsonpath='{.status.httpsProxy}')
-export xNO_PROXY=$(oc get proxy cluster -n openshift-config -ojsonpath='{.status.noProxy}')
+export _HTTP_PROXY=$(oc get proxy cluster -n openshift-config -ojsonpath='{.status.httpProxy}')
+export _HTTPS_PROXY=$(oc get proxy cluster -n openshift-config -ojsonpath='{.status.httpsProxy}')
+export _NO_PROXY=$(oc get proxy cluster -n openshift-config -ojsonpath='{.status.noProxy}')
 
 set_deployment_env() {
   kubectl patch deployment $1 -n $3 \
--p '{"spec":{"template":{"spec":{"containers":[{"env":[{"name":"HTTPS_PROXY","value":"'${xHTTPS_PROXY}'"},{"name":"HTTP_PROXY","value":"'${xHTTP_PROXY}'"},{"name":"NO_PROXY","value":"'${xNO_PROXY}'"}],"name":"'$2'"}]}}}}'
+-p '{"spec":{"template":{"spec":{"containers":[{"env":[{"name":"HTTPS_PROXY","value":"'${_HTTPS_PROXY}'"},{"name":"HTTP_PROXY","value":"'${_HTTP_PROXY}'"},{"name":"NO_PROXY","value":"'${_NO_PROXY}'"}],"name":"'$2'"}]}}}}'
 }
 
 echo "Using these current proxy settings fromt the current cluster proxy settings ..."
-echo "  HTTP_PROXY: ${xHTTP_PROXY}"
-echo "  HTTPS_PROXY: ${xHTTPS_PROXY}"
-echo "  NO_PROXY: ${xNO_PROXY}"
+echo "  HTTP_PROXY: ${_HTTP_PROXY}"
+echo "  HTTPS_PROXY: ${_HTTPS_PROXY}"
+echo "  NO_PROXY: ${_NO_PROXY}"
 echo ""
 
-# Pause Hub Management
+# Pause the hub management of the klusterlet agent and addon operator for hub, and all managed clusters across the fleet
 # oc annotate -n open-cluster-management `oc get mch -oname -n open-cluster-management | head -n1` mch-pause=true --overwrite=true
 
-# Pause Spoke Management on local-cluster
 # oc annotate klusterletaddonconfig -n local-cluster local-cluster klusterletaddonconfig-pause=false --overwrite=true
 oc annotate klusterletaddonconfig -n local-cluster local-cluster klusterletaddonconfig-pause=true --overwrite=true
-echo "Paused the managed cluster addonconifg on: local-cluster"
+echo "Paused the auto management of the addon components on: local-cluster."
 echo ""
 
 for NS in open-cluster-management-agent open-cluster-management-agent-addon
 do
   list=`oc get deployment -n $NS --no-headers -o custom-columns=":metadata.name"`
-  for y in $list
+  for deployment in $list
   do
-    if oc get deployment $y -n $NS -o yaml | grep "HTTP_PROXY" > /dev/null; then
+    if oc get deployment $deployment -n $NS -o yaml | grep "HTTP_PROXY" > /dev/null; then
       :
     else
-      echo "updating deployment: $y"
+      echo "updating deployment: $deployment"
       if [ ! -z $DEBUG ]; then
-        oc get deployment $y -n $NS -ojsonpath='{.spec.template.spec.containers}' | jq . | grep '\"name\":'
+        oc get deployment $deployment -n $NS -ojsonpath='{.spec.template.spec.containers}' | jq . | grep '\"name\":'
       fi    
-      c=$(oc get deployment $y -n $NS -ojsonpath='{.spec.template.spec.containers}' | jq . | grep '^    \"name\":' | awk '{print $2}' | tr -d "\"" | tr -d ",")
+      c=$(oc get deployment $deployment -n $NS -ojsonpath='{.spec.template.spec.containers}' | jq . | grep '^    \"name\":' | awk '{print $2}' | tr -d "\"" | tr -d ",")
       for container_name in $c
       do
-        echo $y, $container_name, $NS
-        #if [[ $y == "klusterlet" || 
-        #      $y == "klusterlet-work-agent" ||
-        #      $y == "klusterlet-registration-agent"
-        #]]; then
-        #  echo "skipping ...$y"
+        # echo $deployment, $container_name, $NS
+        # if [[ $deployment == "klusterlet" || 
+        #      $deployment == "klusterlet-work-agent" ||
+        #      $deployment == "klusterlet-registration-agent"
+        # ]]; then
+        #  echo "skipping $deployment ..."
         #  continue
-        #fi
-        set_deployment_env $y $container_name $NS
+        # fi
+        set_deployment_env $deployment $container_name $NS
       done
       echo ""
     fi
